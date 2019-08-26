c76d0d519ff87c99097dab05b70296f2dee4c215
Jamie Carter <48565047+jc2820@users.noreply.github.com>
2019-08-26 16:46:36 +0100

    Add new image paths to carousel function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -66,7 +66,7 @@
 
 
             //Carousel functions
-            var images = ["02.png", "03.png", "01.png"];
+            var images = ["images/02.png", "images/03.png", "images/01.png"];
             var i = 0;
             var timer = setInterval(next, 1000);
 
